f07d12c6f9573a46357864a908f94606a5710bcf
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-09-16 08:50:04 -0400

    Add a TextInput and a Button to the App.js functional component

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.screen}>
+
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input}/>
+        <Button title="ADD" />
+      </View>
+
+      <View>
+      
+      </View>
+    
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: "row", justifyContent: "space-between", alignItems: "center"
+  },
+  input: { 
+    width: "70%", 
+    borderColor: "black", borderWidth: 1, 
+    padding: 10
+  }
 });
