d0a152728ba99a96c84472cca83895c2922d8bcc
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-09-20 18:27:27 -0400

    Add functionality of removing a Goal Item from the courseGoals state

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, Button, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
-
-
   const [courseGoals, setCourseGoals] = useState([]);
 
   const addGoalHandler = goalTitle => {
-      setCourseGoals(currentGoals => [...currentGoals, { key: Math.random().toString(), value: goalTitle}]);
+      setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goalTitle}]);
+  }
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId)
+    });
   }
 
   return (
@@ -17,7 +21,20 @@ export default function App() {
 
         <GoalInput onAddGoal={addGoalHandler}/>
 
-        <FlatList data={courseGoals} renderItem={itemData => <GoalItem title={itemData.item.value}/>}
+        <FlatList 
+          data={courseGoals}
+
+          keyExtractor={(item, index) => item.id}
+          
+          renderItem={itemData => 
+            <GoalItem              
+              id={itemData.item.id}
+
+              onDelete={removeGoalHandler}
+
+              title={itemData.item.value}
+              />
+          }
         />
 
     </View>
@@ -27,7 +44,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-
-
+  }
 });
